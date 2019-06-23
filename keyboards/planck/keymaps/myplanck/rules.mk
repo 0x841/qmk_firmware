a2d228cbc054b2baf6c88e8563364e373df965c2
oka-h <hirokioka0828@gmail.com>
2019-06-23 23:24:46 +0900

    [update] Add new keymap

--- keyboards/planck/keymaps/myplanck/rules.mk
+++ keyboards/planck/keymaps/myplanck/rules.mk
@@ -2,10 +2,12 @@ ifndef QUANTUM_DIR
   include ../../../../Makefile
 endif
 
-AUDIO_ENABLE     = no
-BACKLIGHT_ENABLE = yes
-
 USER_NAME := oka-h
 
-SRC += ./keymaps/myplanck/audio_user.c \
-       ./keymaps/myplanck/backlight_user.c
+AUDIO_ENABLE = no
+SRC += ./keymaps/myplanck/audio_user.c
+
+ifneq ($(RGB_MATRIX_ENABLE), yes)
+BACKLIGHT_ENABLE = yes
+SRC += ./keymaps/myplanck/backlight_user.c
+endif
