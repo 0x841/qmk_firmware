4316774e7c6eb0ed3bbc53c40071783981f0ef50
oka-h <hirokioka0828@gmail.com>
2020-12-28 21:40:20 +0900

    [fix]

--- keyboards/crkbd/keymaps/mycrkbd/rules.mk
+++ keyboards/crkbd/keymaps/mycrkbd/rules.mk
@@ -29,7 +29,6 @@ SRC += ./lib/glcdfont.c \
        ./lib/layer_state_reader.c \
        ./lib/logo_reader.c \
        ./lib/keylogger.c \
-       # ./keymaps/mycrkbd/oled.c
        # ./lib/mode_icon_reader.c \
        # ./lib/host_led_state_reader.c \
        # ./lib/timelogger.c \
